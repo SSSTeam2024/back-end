311bb1a7fd801ce4cb9d6d2161bc7f1fbed314c4
SSSTeam2024 <sss.team2024@gmail.com>
2024-11-13 10:34:55 +0100

    update to coachhirenetwork.co.uk/

--- controllers/quoteController/quoteController.js
+++ controllers/quoteController/quoteController.js
@@ -392,7 +392,7 @@ const updateQuoteStatus = async (req, res) => {
 
     res.json({
       link:
-        "http://www.coachhirenetwork.co.ukBooking-Success.html?type=" +
+        "http://www.coachhirenetwork.co.uk/Booking-Success.html?type=" +
         payment_type +
         "&mode=" +
         payment_mode +
